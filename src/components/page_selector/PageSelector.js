24337d4439709f3fb0ff30e5f4a6643531aa9252
cooljasonmelton <cooljasonmelton>
2020-08-01 02:18:40 -0500

    installed router working on page selector for tabs

--- src/components/page_selector/PageSelector.js
+++ src/components/page_selector/PageSelector.js
@@ -4,8 +4,11 @@ import './PageSelector.css';
 const PageSelector = () => {
   return (
     <div className="PageSelector">
-      selector
-
+      <div className="selector-item">Demographic Info</div>
+      <div className="selector-item">Medical History</div>
+      <div className="selector-item">Medications/Allergies</div>
+      <div className="selector-item">Family Health History</div>
+      <div className="selector-item">Emergency Contacts</div>
     </div>
   );
 }
