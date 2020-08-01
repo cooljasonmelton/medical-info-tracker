4a15a0909019213ea302db94517a4d78844d3acb
cooljasonmelton <cooljasonmelton>
2020-08-01 03:00:47 -0500

    prepared for react router

--- src/components/page_selector/PageSelector.js
+++ src/components/page_selector/PageSelector.js
@@ -4,10 +4,9 @@ import React, { useState } from 'react';
 import './PageSelector.css';
 
 const PageSelector = () => {
-  const [active, setActive] = useState('')
+  const [active, setActive] = useState('Demographic Info')
 
-  console.log(active)
-
+  // array of page names
   const pageNames = [
     "Demographic Info",
     "Medications / Allergies",
@@ -16,17 +15,24 @@ const PageSelector = () => {
     "Emergency Contacts"
   ];
 
+  // sets class names, adds active if page matches state
   const setClassNames = page => {
     const classNameArr = ["selector-item"];
     if (active == page) classNameArr.push('active')
     return classNameArr.join(' ');
   };
 
+  const handleClick = e => {
+    setActive(e.target.innerText)
+    // add push to route to change med info
+  }
+
+  // maps pageNames into h3s 
   const renderSelectors = () => {
     return pageNames.map(page=> {
     return (
       <h3 className={setClassNames(page)}
-        onClick={e => setActive(e.target.innerText)}> 
+        onClick={handleClick}> 
         {page} 
       </h3>
       )
