46aa803a3d8fa7166958a9d16a3a1c98fcb7279d
cooljasonmelton <cooljasonmelton>
2020-08-02 20:42:32 -0500

    set up functionality for emergency contact comp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 // router 
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 // styles
 import './App.css';
@@ -10,6 +10,7 @@ import './App.css';
 import PageSelector from './components/page_selector/PageSelector';
 import MedInfoContainer from './components/med_info_container/MedInfoContainer';
 import Demographics from './components/info_components/Demographics';
+import EmergencyContacts from './components/info_components/EmergencyContacts';
 
 const App = () => {
   return (
@@ -18,12 +19,14 @@ const App = () => {
         <PageSelector/>
         <MedInfoContainer/>
 
+        <Redirect from="/" to="/demographics" />
+
         <Switch>
             <Route exact path="/demographics" component={Demographics} />
             <Route exact path="/" component={null} />
             <Route exact path="/" component={null} />
             <Route exact path="/" component={null} />
-            <Route exact path="/" component={null} />
+            <Route exact path="/emergency-contacts" component={EmergencyContacts} />
           </Switch>
       </div>
     </Router>
---
+++ src/components/info_components/EmergencyContacts.js
@@ -0,0 +1,59 @@
+import React, {useState} from 'react';
+//styling
+import './InfoComponents.css';
+
+//components
+import ItemDisplayEdit from '../../functions/ItemDisplayEdit'
+// import {renderPageItems} from '../../functions/renderPageItems'
+
+
+const EmergencyContacts = () => {
+  // changes text to inputs for editing
+  const [edit, setEdit] = useState(false)
+  const [numContact, setNumContact] = useState(1)
+
+  // form items
+  const emerencyContactItems = [
+    "Name", 
+    "Relationship",
+    "Phone Number",
+    "Address",
+  ]
+
+  // determines number of contacts based on numContact state
+  const renderContactBox = () => {
+    const contactArr = []
+    for (let i = 0; i < numContact; i++){
+        contactArr.push(
+            <>
+                <h3 className="info-header">Contact {i+1}</h3>
+                {emerencyContactItems.map(label => <ItemDisplayEdit label={label} edit={edit}/>)}
+            </>
+        )
+    }
+    return contactArr
+  }
+
+
+
+  return (
+    <div className="EmergencyContacts info-comp">
+
+      <h1 className="info-header">Emergency Contacts</h1>
+      <button className="edit-button" 
+        onClick={() => setEdit(!edit)}>
+        Edit
+      </button>
+
+      {renderContactBox()}
+
+      <button onClick={() => setNumContact(numContact + 1)}>
+          Add Contact
+      </button>
+
+
+    </div>
+  );
+}
+
+export default EmergencyContacts;
